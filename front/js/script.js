e21923b271a23b343de50e659763a8bfdd7487a3
Louis Hamelin <louishamelin244@gmail.com>
2022-09-13 20:08:20 +0200

    Page script terminée et ajout des autres pages javascript

---
+++ front/js/script.js
@@ -0,0 +1,20 @@
+let url = `http://localhost:3000/api/products`;
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        let affichage = "";
+        for (let product of data){
+            affichage += `<a href="./product.html?id=${product._id}">
+            <article>
+              <img src="${product.imageUrl}" alt="${product.altTxt}">
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>
+            </article>
+          </a>`
+        }
+        console.log(document.querySelector("#items"));
+        document.querySelector("#items").innerHTML = affichage;
+    })
+    .catch(error => {console.error(error)});
