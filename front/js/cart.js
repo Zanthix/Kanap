79bfc5ffe97f87e3ac50ad4873e7354ce9e59fd1
Louis Hamelin <louishamelin244@gmail.com>
2022-09-27 20:02:04 +0200

    Ajout des articles dans le panier (check de sécurité à faire)

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,5 @@
-
+const nouveau = localStorage.getItem('price');
+console.log(nouveau);
 
 
 
--- front/js/product.js
+++ front/js/product.js
@@ -7,27 +7,60 @@ console.log(url);
 
 
 fetch(url)   
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        if(data._id != id){
-          console.log("ERREUR");
-          return('Id non valide');
+  .then(response => response.json())
+  .then(data => {
+      console.log(data);
+      //Check si l'id de la barre recherche correspond à l'id du produit
+      if(data._id != id){
+        console.log("ERREUR");
+        return('Id non valide');
+      }
+      //Création des différentes variables à injecter dans le DOM
+      let option = `<option value="">--SVP, choisissez une couleur --</option>`;
+      let picture = `<img src="${data.imageUrl}" alt="${data.altTxt}"> `;
+      let nameProduit = `<h1 id='title'>${data.name}</h1>`;
+      let price = `<span id="price">${data.price}</span>`;
+      let desc = `<p id="description">${data.description}</p>`;
+
+      //Création des différentes options pour les couleurs de chaque produit
+      for(let product of data.colors){
+        option += `<option value="${product}">${product}</option>`
+      }
+
+      //Injection des différentes variables dans le DOM
+      document.querySelector('.item__img').innerHTML = picture;
+      document.querySelector('#title').innerHTML = nameProduit;
+      document.querySelector('#price').innerHTML = price;
+      document.querySelector('#description').innerHTML = desc;
+      document.querySelector('#colors').innerHTML = option;
+      
+
+
+      document.querySelector('#addToCart').addEventListener('click', () =>{
+        const color = getValueFromField('colors');
+        const quantity = parseInt(getValueFromField('quantity'));
+        console.log(color,quantity);
+        const product = {id, color, quantity}        
+        let products = JSON.parse(localStorage.getItem('products'));
+        if (products === null) {
+          products = [];
         }
-        let options = `<option value="">--SVP, choisissez une couleur --</option>`;
-        let picture = `<img src="${data.imageUrl}" alt="${data.altTxt}"> `;
-        let nameProduit = `<h1 id='title'>${data.name}</h1>`;
-        let price = `<span id="price">${data.price}</span>`;
-        let desc = `<p id="description">${data.description}</p>`;
-        for(let product of data.colors){
-          options += `<option value="${product}">${product}</option>`
+        for (let i = 0; i <products.length; i++) {
+          if (products[i].id === id && products[i].color === color){
+            console.log('Présent');
+            products[i].quantity += quantity;
+          }else{
+            console.log('Absent');
+            products.push(product);
+          }
         }
-        document.querySelector('.item__img').innerHTML = picture;
-        document.querySelector('#title').innerHTML = nameProduit;
-        document.querySelector('#price').innerHTML = price;
-        document.querySelector('#description').innerHTML = desc;
-        document.querySelector('#colors').innerHTML = options;
-    }).catch(error => {console.log(error)});
+        localStorage.setItem('products', JSON.stringify(products));
+      })
+  }).catch(error => {console.log(error)});
+
+const getValueFromField = (id) => {
+  return document.getElementById(id).value;
+}
 
 
 
